0feb89a62f93cfb5bc8c9a802d4068104d654132
Yevgeni Shapiro <cloudtron.inc@gmail.com>
2023-03-22 22:24:13 +0200

    Initial commit

--- nginx.tf
+++ nginx.tf
@@ -1,4 +1,4 @@
-# nginx_ingress.tf
+# Install traefik helm_char
 
 provider "helm" {
   kubernetes {
@@ -6,35 +6,42 @@ provider "helm" {
   }
 }
 
-resource "helm_release" "ingress_nginx" {
-  name       = "ingress-nginx"
-  repository = "https://kubernetes.github.io/ingress-nginx"
-  chart      = "ingress-nginx"
-  version    = var.ingress_nginx_helm_version
 
-  namespace        = var.ingress_nginx_namespace
+resource "helm_release" "traefik" {
+  namespace        = var.namespace
   create_namespace = true
-  timeout = 300
+  name             = "traefik"
+  repository       = "https://traefik.github.io/charts"
+  chart            = "traefik"
+  version          = var.traefik_chart_version
 
-  values = [file("config/nginx_values.yaml")]
+  # Helm chart deployment can sometimes take longer than the default 5 minutes
+  timeout = var.timeout_seconds
 
-  depends_on = [kind_cluster.default]
+  # If values file specified by the var.values_file input variable exists then apply the values from this file
+  # else apply the default values from the chart
+  values = [fileexists("${path.root}/${var.values_file}") == true ? file("${path.root}/${var.values_file}") : ""]
+
+  set {
+    name  = "deployment.replicas"
+    value = var.replica_count
+  }
 }
 
-resource "null_resource" "wait_for_ingress_nginx" {
+resource "null_resource" "wait_for_traefik" {
   triggers = {
     key = uuid()
   }
 
   provisioner "local-exec" {
     command = <<EOF
-      printf "\nWaiting for the nginx ingress controller...\n"
-      kubectl wait --namespace ${helm_release.ingress_nginx.namespace} \
+      printf "\nWaiting for the traefik ingress controller...\n"
+      kubectl wait --namespace ${helm_release.traefik.namespace} \
         --for=condition=ready pod \
         --selector=app.kubernetes.io/component=controller \
         --timeout=90s
     EOF
   }
 
-  depends_on = [helm_release.ingress_nginx]
+  depends_on = [helm_release.traefik]
 }
